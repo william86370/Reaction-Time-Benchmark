1c39eb1d9f646a14233c88d87174d2640c344f78
William Wright <william86378@williams-mac.local>
2016-02-13 15:38:08 -0800

    Initial Commit

---
+++ reaction time benchmark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reaction time benchmark
+//
+//  Created by william wright  on 2/13/16.
+//  Copyright © 2016 A.R.C software and enginering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
